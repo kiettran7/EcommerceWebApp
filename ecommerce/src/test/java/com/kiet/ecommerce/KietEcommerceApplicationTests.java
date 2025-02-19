05c28bd3dc7c1d1435516bfe325f3022af2e7301
Kiet <2151050219kiet@ou.edu.vn>
2025-02-19 23:27:27 +0700

    Init my project

---
+++ ecommerce/src/test/java/com/kiet/ecommerce/KietEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiet.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KietEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
